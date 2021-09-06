d3c2833f5675bfbb72d83b39cb5ec3f5fa92f308
siddharth952 <siddharth952@gmail.com>
2021-09-06 18:19:58 +0530

    Complete frontend

--- fam_django/fam_django/urls.py
+++ fam_django/fam_django/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 # / by default will redirect both with/without / routes
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('search.urls')),
+    path('', include('home.urls')),
 ]
---
+++ fam_django/home/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+
+# Test
+videos = [
+    {
+        'title': 'Unboxing the new Ps5',
+        'author': 'UnboxingTherpy',
+        'date_uploaded': '03-2-19'
+    },
+    {
+        'title': 'Unboxing the new Xbox',
+        'author': 'UnboxingTherpy',
+        'date_uploaded': '03-2-20'
+    },
+    {
+        'title': 'Unboxing the new Pc',
+        'author': 'UnboxingTherpy',
+        'date_uploaded': '03-2-22'
+    },
+    {
+        'title': 'Unboxing the new Wii',
+        'author': 'UnboxingTherpy',
+        'date_uploaded': '13-5-21'
+    },
+]
+
+
+
+
+def home(request):
+    # Let us access in the template, info of each video
+    context = {
+        'videos': videos
+    }
+    return render(request, 'home/home.html', context)
