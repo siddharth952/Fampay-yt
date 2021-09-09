95dc1816a355784992e74932c5aec34790811cd4
siddharth952 <siddharth952@gmail.com>
2021-09-09 13:39:13 +0530

    Fix issue with type of data for the videos

--- fam_django/home/views.py
+++ fam_django/home/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .yt_session import getChannelStatus, getListVideos
 from .models import Video
 from .managedatabase import *
-
+#from django.core.paginator import Paginator  
 
 def home(request):
     context = {}
@@ -12,14 +12,20 @@ def home(request):
         user_search_data = request.POST['searched']
         
         # Let us access in the template, info of each video
-        res = getListVideos(user_search_data)['items']
-
+        data = getListVideos(user_search_data)
+        res = data['items']
+        
+        # For Pagination
+        # page = request.GET.get('page')
+        # next_page_token = data['nextPageToken']
+        # if page > 1:
+        #     prev_page_token = data['prevPageToken']
+        
+        
         # Clear the database
         delete_videos_database()
         # Add to database
         add_to_database(res)
-        
-        
         
         context = {
         'videos': sort_filter_videos(sort_by='publishTime'),
